5b83adfe38190bb4821a06ab26ba1352cf59a394
Chathura-Nimsara <99wakcnk@gmail.com>
2023-03-24 13:25:57 +0530

    initial comment

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Loginform from "./login-component/loginform";
-import Signup from "./signup-component/signup";
-import Page from "./page-component/page";
+import Innovative from "./innovative-component/innovative";
 
 function App() {
   return (
-<<<<<<< Updated upstream
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Loginform />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/page" element={<Page />} />
+          <Route path="/" element={<Innovative/>} />
         </Routes>
       </Router>
     </div>
-=======
-  <div className= "App">
-    
-    <Signup/>
-  </div>
-   
->>>>>>> Stashed changes
+
   );
 }
 
---
+++ src/innovative-component/innovative.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./innovative.css";
+
+
+const Innovative = () => {
+ 
+  return (
+    <div className="box-style"></div>
+  );
+};
+
+export default Innovative;
