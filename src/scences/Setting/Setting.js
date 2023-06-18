db04d940cc9460e01b6df1277c0fe35872241b7f
Chiran Sooryakumara <85780849+Fernandophcsg@users.noreply.github.com>
2023-06-18 18:18:09 +0530

    settings page update

--- src/scences/Setting/Setting.js
+++ src/scences/Setting/Setting.js
@@ -1,32 +1,32 @@
-import React, { useState,useEffect } from 'react';
-import axios from 'axios';
-import { Box, CssBaseline } from '@mui/material';
-import NavBar from '../../components/NavBar/NavBar';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Box, CssBaseline } from "@mui/material";
+import NavBar from "../../components/NavBar/NavBar";
 
 export const Setting = () => {
-
   const [userData, setUserData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    city: 'None',
-    phoneNum: '',
-    description:'None',
+    firstName: "",
+    lastName: "",
+    email: "",
+    city: "",
+    phoneNum: "",
+    description: "",
     profilePicture: null,
   });
-  const [data, setData] = useState([])
-  const [error, setError] = useState(false)
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    axios.get('https://localhost:7224/api/Users')
-      .then(res => {
-        setData(res.data)
-      }).catch(err => {
-        console.log(err)
-        setError(true)
+    axios
+      .get("https://localhost:7224/api/Users")
+      .then((res) => {
+        setData(res.data);
       })
-  }, [])
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
+  }, []);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -34,109 +34,183 @@ export const Setting = () => {
       ...userData,
       [name]: value,
     });
+    userData.city = event.target.value;
+    userData.phoneNo = event.target.value;
+    userData.description = event.target.value;
   };
 
-  const handleFileChange = (event) => {
-    setUserData({
-      ...userData,
-      profilePicture: event.target.files[0],
-    });
-  };
+  const fname = data
+    .filter((item) => item.uId === 2)
+    .map((item) => item.lastName);
+  const lname = data
+    .filter((item) => item.uId === 1)
+    .map((item) => item.lastName);
+  const email = data.filter((item) => item.uId === 2).map((item) => item.email);
+  const backendcity = data.filter((item) => item.uId === 2).map((item) => item.city);
+  const backendphoneNo = data
+    .filter((item) => item.uId === 2)
+    .map((item) => item.phoneNo);
+  const backendDescription = data
+    .filter((item) => item.uId === 2)
+    .map((item) => item.description);
 
+  
   const handleSubmit = (event) => {
     event.preventDefault();
+    const formData = {
+      uId: 1,
+      firstName: "string",
+      lastName: "string",
+      email: "string",
+      city : userData.city,
+      phoneNo : userData.phoneNo,
+      description : userData.description,
+      role: 0
+    }
 
-    // Create a new FormData object to send the user data and profile picture
-    const formData = new FormData();
-    formData.append('firstName', userData.firstName);
-    formData.append('lastName', userData.lastName);
-    formData.append('email', userData.email);
-    formData.append('city', userData.city);
-    formData.append('phoneNo', userData.city);
-    formData.append('description', userData.description);
-    formData.append('profilePicture', userData.profilePicture);
-
-    // Make a POST request to the backend API to update the user data
-    axios.post('/api/updateUser', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
+    // Make a Update request to the backend API to update the user data
+    axios
+      .put('https://localhost:7224/api/Users/2', formData)
       .then((response) => {
-        console.log(response.data);
+        alert('change successful')
+        window.location.reload();
       })
       .catch((error) => {
         console.log(error);
+        alert("can't change value error happen")
       });
   };
 
   const styles = {
     label: {
-      display: 'block',
-      margin: '10px 0',
-      fontWeight: 'bold',
+      display: "block",
+      margin: "10px 0",
+      fontWeight: "bold",
     },
     input: {
-      display: 'block',
-      width: '90%',
-      padding: '10px',
-      border: '1px solid #ccc',
-      borderRadius: '5px',
-      fontSize: '16px',
-      marginBottom: '20px',
+      display: "block",
+      width: "90%",
+      padding: "10px",
+      border: "1px solid #ccc",
+      borderRadius: "5px",
+      fontSize: "16px",
+      marginBottom: "20px",
     },
     button: {
-      backgroundColor: '#4CAF50',
-      color: 'white',
-      padding: '10px 20px',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-      fontSize: '16px',
-      marginBottom: '20px',
+      backgroundColor: "#4CAF50",
+      color: "white",
+      padding: "10px 20px",
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+      fontSize: "16px",
+      marginBottom: "20px",
     },
   };
-  const fname = data.filter(item => item.uId === 2 ).map((item, index) => (item.lastName))
-  const lname = data.filter(item => item.uId === 1 ).map((item, index) => (item.lastName))
-  const email = data.filter(item => item.uId === 2  ).map((item, index) => (item.email))
+  
   return (
     <>
-    <Box sx={{ display: 'flex' }} >
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <NavBar />
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 0,
+            backgroundColor: "#f9f9f9",
+            borderRadius: "5px",
+            padding: "30px 0 0 20px",
+          }}
+        >
+          <Box sx={{ m: "30px 180px" }}>
+            <form onSubmit={handleSubmit}>
+              <label style={styles.label} htmlFor="firstName">
+                First Name:
+              </label>
+              <input
+                style={styles.input}
+                type="text"
+                name="firstName"
+                value={userData.firstName}
+                onChange={handleInputChange}
+                placeholder={fname}
+                disabled
+              />
 
-    <CssBaseline/>
-    <NavBar/>
-    <Box component="main" sx={{ flexGrow: 1, p: 0, backgroundColor: '#f9f9f9', borderRadius: '5px',padding:'70px 0 0 20px' }}>
+              <label style={styles.label} htmlFor="lastName">
+                Last Name:
+              </label>
+              <input
+                style={styles.input}
+                type="text"
+                name="lastName"
+                value={userData.lastName}
+                onChange={handleInputChange}
+                placeholder={lname}
+                disabled
+              />
 
-    <Box sx={{m:'30px 180px'}}>
+              <label style={styles.label} htmlFor="email">
+                Email Address:
+              </label>
+              <input
+                style={styles.input}
+                type="email"
+                name="email"
+                value={userData.email}
+                onChange={handleInputChange}
+                placeholder={email}
+                disabled
+              />
 
-    <form onSubmit={handleSubmit}>
-      <label style={styles.label} htmlFor="firstName">First Name:</label>
-      <input style={styles.input} type="text" name="firstName" value={userData.firstName} onChange={handleInputChange} placeholder={fname} disabled/>
+              <label style={styles.label} htmlFor="city">
+                City/Town:
+              </label>
+              <input
+                style={styles.input}
+                type="text"
+                name="city"
+                placeholder={backendcity}
+                value={userData.city}
+                onChange={handleInputChange}
+              />
 
-      <label style={styles.label} htmlFor="lastName">Last Name:</label>
-      <input style={styles.input} type="text" name="lastName" value={userData.lastName} onChange={handleInputChange} placeholder={lname} disabled/>
+              <label style={styles.label} htmlFor="phoneNo">
+                phone Number:
+              </label>
+              <input
+                style={styles.input}
+                type="text"
+                name="phoneNo"
+                placeholder={backendphoneNo}
+                value={userData.phoneNo}
+                onChange={handleInputChange}
+              />
 
-      <label style={styles.label} htmlFor="email">Email Address:</label>
-      <input style={styles.input} type="email" name="email" value={userData.email} onChange={handleInputChange} placeholder={email} disabled />
+              <label style={styles.label} htmlFor="description">
+                Description:
+              </label>
+              <textarea
+                style={styles.input}
+                type="textarea"
+                name="description"
+                placeholder={backendDescription}
+                value={userData.description}
+                onChange={handleInputChange}
+              />
 
-      <label style={styles.label} htmlFor="city">City/Town:</label>
-      <input style={styles.input} type="text" name="city" value={userData.city} onChange={handleInputChange} />
-
-      <label style={styles.label} htmlFor="phoneNo">phone Number:</label>
-      <input style={styles.input} type="text" name="phoneNo" value={userData.phoneNo} onChange={handleInputChange} />
-
-      <label style={styles.label} htmlFor="description">Description:</label>
-      <textarea style={styles.input} type="textarea" name="description" value={userData.description} onChange={handleInputChange} />
-
-      <label style={styles.label} htmlFor="profilePicture">Profile Picture:</label>
-      <input style={styles.input} type="file" name="profilePicture" onChange={handleFileChange} />
-
-      <button style={styles.button} type="submit">Save Changes</button>
-    </form>
-    </Box>
-    </Box>
-    </Box>
+              <button
+                style={styles.button}
+                type="button"
+                onClick={handleSubmit}
+              >
+                Save Changes
+              </button>
+            </form>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
-  }
-
+};
